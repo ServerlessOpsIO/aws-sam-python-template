efdf6b5e4a9db7f02fb8586fc7d37f4ad0de0755
Tom McLaughlin <tom@serverlessops.io>
2019-05-31 11:38:07 -0400

    Initial commit

---
+++ {{cookiecutter.service_name}}/src/common/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import io
+import re
+import os
+from setuptools import setup, find_packages
+
+module_name = "{{cookiecutter.service_name}}"
+
+setup(
+    name='{{cookiecutter.service_name}}-common',
+    version='0.0.1',
+    description='{{cookiecutter.service_name}} Service Common Code',
+    author='{{cookiecutter.author_name}}',
+    author_email='{{cookiecutter.author_email}}',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=['tests.*', 'tests']),
+    keywords="{{cookiecutter.service_name}} Service",
+    python_requires='>=3.7.*',
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
+
---
+++ {{cookiecutter.service_name}}/src/handlers/{{cookiecutter.function_name}}/__init__.py
@@ -0,0 +1,39 @@
+'''{{cookiecutter.function_description}}'''
+
+import json
+import logging
+import os
+
+{%- if cookiecutter.enable_xray == "y" %}
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+# Only instrument libraries if not running locally
+if "AWS_SAM_LOCAL" not in os.environ:
+    patch_all()
+{%- endif %}
+
+
+from  ... import common
+
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+logging.root.setLevel(logging.getLevelName(log_level))  # type: ignore
+_logger = logging.getLogger(__name__)
+
+def handler(event, context):
+    '''Function entry'''
+    _logger.debug('Event: {}'.format(json.dumps(event)))
+
+{%- if cookiecutter.enable_xray == "y" %}
+    resp = {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+        }),
+    }
+{%- else -%}
+    resp = {}
+{%- endif -%}
+
+    _logger.debug('Response: {}'.format(json.dumps(resp)))
+    return resp
+
