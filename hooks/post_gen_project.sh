a33261cc97ee8326969f3a0f9b806e62b0eac779
Tom McLaughlin <tom@serverlessops.io>
2020-03-28 16:50:53 -0400

    udpate usage
    * chase updates to CLI usahe
    * create samconfig.toml
    * update Makefile accordingly

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,3 +3,9 @@
 # Initialize project
 make init
 
+# Set S3 Bucket location by querying S3
+echo "version=0.1" > samconfig.toml
+echo "[default.deploy.parameters]" >> samconfig.toml
+echo "capabilities = \"CAPABILITY_IAM\"" >> samconfig.toml
+echo "s3_bucket = \"$(aws cloudformation describe-stack-resource --stack-name aws-sam-cli-managed-default --logical-resource-id SamCliSourceBucket | jq -r'.StackResourceDetail.PhysicalResourceId')\"" >> samconfig.toml
+
