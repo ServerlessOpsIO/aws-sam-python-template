640fd240cc2fedad774f28e40fce12113ff21542
Tom McLaughlin <tom@serverlessops.io>
2020-03-30 10:24:44 -0400

    minor pylint and flake 8 stuff

--- {{cookiecutter.service_name}}/src/handlers/{{cookiecutter.function_name}}/function.py
+++ {{cookiecutter.service_name}}/src/handlers/{{cookiecutter.function_name}}/function.py
@@ -13,10 +13,10 @@ if "AWS_SAM_LOCAL" not in os.environ:
 
 # This path reflects the packaged path and not repo path to the common
 # package for this service.
-import common
+import common   # pylint: disable=unused-import
 
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
-logging.root.setLevel(logging.getLevelName(log_level))  # type: ignore
+logging.root.setLevel(logging.getLevelName(log_level))
 _logger = logging.getLogger(__name__)
 
 def handler(event, context):
