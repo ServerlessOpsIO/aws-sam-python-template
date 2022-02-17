c0524561ef612656f9566b71c77a578d4dfb4bc3
Tom McLaughlin <tom@serverlessops.io>
2022-02-17 16:43:47 -0500

    Don't need to set S3 bucket anymore for SAM

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,9 +3,6 @@
 # Initialize project
 make init
 
-# Set S3 Bucket location by querying S3
-echo "s3_bucket = \"$(aws cloudformation describe-stack-resource --stack-name aws-sam-cli-managed-default --logical-resource-id SamCliSourceBucket | jq -r'.StackResourceDetail.PhysicalResourceId')\"" >> samconfig.toml
-
 # Initialize git
 git init
 git add ./
