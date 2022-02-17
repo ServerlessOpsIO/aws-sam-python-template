f5e9f346c40e8aedf4576b0a74d406a135a0cc2e
Tom McLaughlin <tom@serverlessops.io>
2022-02-17 15:25:39 -0500

    don't fully overrwrite samconfig.toml

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,9 +4,6 @@
 make init
 
 # Set S3 Bucket location by querying S3
-echo "version=0.1" > samconfig.toml
-echo "[default.deploy.parameters]" >> samconfig.toml
-echo "capabilities = \"CAPABILITY_IAM\"" >> samconfig.toml
 echo "s3_bucket = \"$(aws cloudformation describe-stack-resource --stack-name aws-sam-cli-managed-default --logical-resource-id SamCliSourceBucket | jq -r'.StackResourceDetail.PhysicalResourceId')\"" >> samconfig.toml
 
 # Initialize git
